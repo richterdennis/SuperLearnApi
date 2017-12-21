74ce7b44a47df78e34a3fc10f84b3b535f9892b5
Oguz <oguz1612@googlemail.com>
2017-12-21 09:51:01 +0100

    add voting body and the suitable adaptation

--- routers/VoteRouter.js
+++ routers/VoteRouter.js
@@ -7,6 +7,13 @@ const router = module.exports = exports = express.Router();
  *
  * @security  AppKeyAuth, TokenAuth
  *
+ * @body*  {Object}  voting object that needs to be created
+ *    {
+ *		"id": 0,  	   | required
+ *      "user_id": 0,  | 
+ *      "score": 1,   <|>
+ *    }
+ *
  * @path*  {integer}  value       Value of the voting (must be 1, 0 or -1)
  * @path*  {integer}  questionId  ID of question to vote
  *
@@ -19,13 +26,29 @@ router.put('/vote/:value/question/:questionId', AppKeyAuth, TokenAuth, _(async f
 	const value = parseInt(req.params.value);
 	const questionId = parseInt(req.params.questionId);
 
+	const votings = req.body;
+
 	if(!questionId) 
 		return res.status(400).end('Invalid ID supplied');
 
 	if(value !== 1 && value !==0 && value !==-1)
 		return res.status(405).end('Invalid input');
 
-	await VoteService.voteQuestion(questionId, value);
+	if(	!votings			||	
+		!votings.id			||	
+		!votings.user_id	||	
+		!votings.score)
+	{
+		return res.status(405).end('Invalid input');
+	}
+	
+	const erg = await VoteService.voteQuestion(votings, questionId, value,{ 
+		processed: true });
+
+	if(!erg)
+		return res.status(404).end('Object not found');
+	
+
 	res.end('Vote successfully updated');
 }));
 
@@ -33,6 +56,13 @@ router.put('/vote/:value/question/:questionId', AppKeyAuth, TokenAuth, _(async f
  * Updates an existing vote or create one
  *
  * @security  AppKeyAuth, TokenAuth
+ *
+ * @body*  {Object}  voting object that needs to be created
+ *    {
+ *		"id": 0,  	   | required
+ *      "user_id": 0,  | 
+ *      "score": 1,   <|>
+ *    }
  *
  * @path*  {integer}  value   Value of the voting (must be 1, 0 or -1)
  * @path*  {integer}  userId  ID of user to vote
@@ -46,12 +76,27 @@ router.put('/vote/:value/user/:userId', AppKeyAuth, TokenAuth, _(async function(
 	const value = parseInt(req.params.value);
 	const userId = parseInt(req.params.userId);
 
+	const votings = req.body;
+
 	if(!userId) 
 		return res.status(404).end('Invalid ID supplied');
 
 	if(value !== 1 && value !==0 && value !==-1)
 		return res.status(405).end('Invalid input');
 
-	await VoteService.voteUser(userId,value);
+	if(	!votings			||	
+		!votings.id			||	
+		!votings.user_id	||	
+		!votings.score)
+	{
+		return res.status(405).end('Invalid input');
+	}
+
+	const erg = await VoteService.voteUser(votings, userId, value,{ 
+		processed: true });
+
+	if(!erg)
+		return res.status(404).end('Object not found');
+
 	res.end('Vote successfully updated');
 }));
