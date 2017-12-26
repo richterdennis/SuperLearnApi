3b31bb3d27c4e98882860b1ee238b2a4b9e44585
Dennis Richter <dennis.richter@iem.thm.de>
2017-12-26 12:48:34 +0100

    Fix put vote user endpoint

--- routers/VoteRouter.js
+++ routers/VoteRouter.js
@@ -34,46 +34,24 @@ router.put('/vote/:value/question/:questionId', AppKeyAuth, TokenAuth, _(async f
  *
  * @security  AppKeyAuth, TokenAuth
  *
- * @body*  {Object}  voting object that needs to be created
- *    {
- *		"id": 0,  	   | required
- *      "user_id": 0,  |
- *      "score": 1,   <|>
- *    }
- *
  * @path*  {integer}  value   Value of the voting (must be 1, 0 or -1)
  * @path*  {integer}  userId  ID of user to vote
  *
  * @response  {200}  Object successfully updated
  * @response  {400}  Invalid ID supplied
- * @response  {404}  Object not found
  * @response  {405}  Invalid input
  */
 router.put('/vote/:value/user/:userId', AppKeyAuth, TokenAuth, _(async function(req, res) {
 	const value = parseInt(req.params.value);
 	const userId = parseInt(req.params.userId);
 
-	const votings = req.body;
+	if(value !== 1 && value !== 0 && value !== -1)
+		return res.status(405).end('Invalid input');
 
 	if(!userId)
-		return res.status(404).end('Invalid ID supplied');
+		return res.status(400).end('Invalid ID supplied');
 
-	if(value !== 1 && value !==0 && value !==-1)
-		return res.status(405).end('Invalid input');
+	await VoteService.voteQuestion(userId, req.currentUser.id, value);
 
-	if(	!votings			||
-		!votings.id			||
-		!votings.user_id	||
-		!votings.score)
-	{
-		return res.status(405).end('Invalid input');
-	}
-
-	const erg = await VoteService.voteUser(votings, userId, value,{
-		processed: true });
-
-	if(!erg)
-		return res.status(404).end('Object not found');
-
-	res.end('Vote successfully updated');
+	res.end('Object successfully updated');
 }));
--- system/VoteService.js
+++ system/VoteService.js
@@ -67,43 +67,65 @@ exports.voteQuestion = async function(questionId, userId, value){
 /**
  * Update a score for an user
  *
- * @param   {Object}  voting     The voting data
- * @param   {Number}  value      voting value for user
- * @param   {Number}  qestionId  The user id
- *
- * @return  {boolean}             success?
+ * @param  {Number}  userId   The question id
+ * @param  {Number}  voterId  The user id which is voting
+ * @param  {Number}  value    Voting value for question (-1, 0, 1)
  */
-exports.voteUser = async function(voting, userId, value){
-	let data = {
-		id:    voting.id,
-		user_id: voting.user_id,
-		score:  value
-	};
+exports.voteUser = async function(userId, voterId, value){
+	let query, data, res, err;
 
-	let queryInsertVoting = `INSERT INTO votings SET ? ON DUPLICATE KEY UPDATE ?`
+	query = `
+		SELECT v.id, v.score
+		FROM votings v
+			JOIN votings_user_rel vu
+				ON v.id = vu.voting_id
+		WHERE
+			vu.user_id = ?
+			AND v.user_id = ?
+	`;
 
-	let queryVotingUserRel = `
-	INSERT INTO votings_user_rel VALUES(?,?)
-	ON DUPLICATE KEY
-	UPDATE votings_user_rel.user_id = ?`
-
-	let queryTwo = `
-	UPDATE user u
-	JOIN votings_user_rel uq
-	ON u.id = uq.user_id
-	JOIN votings v
-	ON v.id = uq.voting_id
-	SET u.score = u.score + ?
-	WHERE u.id = ?`
-
-	let [err, res] = await db.query(queryInsertVoting, [data, data]);
+	[err, res] = await db.query(query, [userId, voterId]);
 	if(err) throw err;
 
-	[err, res] = await db.query(queryVotingUserRel, [data.id, userId, userId]);
-	if(err) throw err;
+	let balanceValue = value;
 
-	[err, res] = await db.query(queryTwo, [data.score, userId]);
-	if(err) throw err;
+	// Voting exists
+	if(res[0]) {
+		balanceValue = value - res[0].score;
 
-	return !!res.changedRows;
+		query = `UPDATE votings SET score = ? WHERE id = ?`;
+
+		[err] = await db.query(query, [value, res[0].id]);
+		if(err) throw err;
+	}
+
+	// Voting exists not
+	else {
+		query = `INSERT INTO votings SET ?`;
+
+		data = {
+			user_id: voterId,
+			score: value
+		};
+
+		[err, res] = await db.query(query, [data]);
+		if(err) throw err;
+
+		const votingId = res.insertId;
+
+		query = `INSERT INTO votings_user_rel SET ?`;
+
+		data = {
+			voting_id: votingId,
+			user_id: userId
+		};
+
+		[err] = await db.query(query, [data]);
+		if(err) throw err;
+	}
+
+	// Balance user score
+	query = `UPDATE user SET score = score + ? WHERE id = ?`;
+	[err] = await db.query(query, [balanceValue, userId]);
+	if(err) throw err;
 }
