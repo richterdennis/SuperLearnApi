c2de890b8de8d094fbc7d2ea008d474420d2d156
Dennis Richter <dennis.richter@iem.thm.de>
2018-03-04 12:42:43 +0100

    Fix:  Method PUT is not allowed in preflight response.

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ api.use(bodyParser.json());
 api.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-App-Key, X-User-Token");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 	next();
 });
 
